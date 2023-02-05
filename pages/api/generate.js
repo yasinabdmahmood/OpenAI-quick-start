fb24c4e2a5cce9d46f1bad238debb7856d36400a
yasinabdmahmood <abd181103@gmail.com>
2023-02-05 22:05:38 +0300

    Edit the request prompt

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -15,21 +15,23 @@ export default async function (req, res) {
     return;
   }
 
-  const animal = req.body.animal || '';
-  if (animal.trim().length === 0) {
-    res.status(400).json({
-      error: {
-        message: "Please enter a valid animal",
-      }
-    });
-    return;
-  }
+  const word = req.body.word;
+  const description = req.body.description;
+  // if (animal.trim().length === 0) {
+  //   res.status(400).json({
+  //     error: {
+  //       message: "Please enter a valid animal",
+  //     }
+  //   });
+  //   return;
+  // }
 
   try {
     const completion = await openai.createCompletion({
       model: "text-davinci-003",
-      prompt: generatePrompt(animal),
+      prompt: generatePrompt(word,description),
       temperature: 0.6,
+      max_tokens: 100,
     });
     res.status(200).json({ result: completion.data.choices[0].text });
   } catch(error) {
@@ -48,15 +50,22 @@ export default async function (req, res) {
   }
 }
 
-function generatePrompt(animal) {
-  const capitalizedAnimal =
-    animal[0].toUpperCase() + animal.slice(1).toLowerCase();
-  return `Suggest three names for an animal that is a superhero.
+function generatePrompt(word,description) {
+  // const capitalizedAnimal =
+  //   animal[0].toUpperCase() + animal.slice(1).toLowerCase();
+//   return `Suggest three names for an animal that is a superhero.
 
-Animal: Cat
-Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
-Animal: Dog
-Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
-Animal: ${capitalizedAnimal}
-Names:`;
+// Animal: Cat
+// Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
+// Animal: Dog
+// Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
+// Animal: ${capitalizedAnimal}
+// Names:`;
+return `A student in english test was asked to give desciption to word ${word} and 
+  he gave this description "${description}" grade his description from 1 to 10 based on 
+  how good of a description he gave, make sure to give a good description of the word as well 
+  when responding assume that you are directly giving the studen the give back by refering
+  to them as you instead of the student`;
+
+
 }
